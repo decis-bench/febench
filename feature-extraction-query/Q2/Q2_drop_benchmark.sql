1ec7930b11bd17c3ef33ee8835bdda9c75ed784a
likunyi <lkythu@163.com>
2022-12-08 17:17:43 +0800

    modify folder name

---
+++ feature-extraction-query/Q2/Q2_drop_benchmark.sql
@@ -0,0 +1,10 @@
+DROP TABLE benchmark;
+DROP TABLE test;
+DROP TABLE train;
+DROP TABLE windforecasts_wf1;
+DROP TABLE windforecasts_wf2;
+DROP TABLE windforecasts_wf3;
+DROP TABLE windforecasts_wf4;
+DROP TABLE windforecasts_wf5;
+DROP TABLE windforecasts_wf6;
+DROP TABLE windforecasts_wf7;
